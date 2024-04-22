d823a66f2b9091c00e7b83548d920b27cbe16354
Purna Shrestha <crush9918@gmail.com>
2024-04-22 16:54:17 +0545

    Create about page

---
+++ dabout.php
@@ -0,0 +1,20 @@
+<?php
+ini_set('display_startup_errors', 1);
+ini_set('display_errors', 1);
+//include ("jpcache/jpcache.php");
+
+define('HOME_PAGE', 1); // Track homepage.
+define('JCMSTYPE', 0); // Track Current site language.
+
+require_once("includes/initialize.php");
+
+$currentTemplate	= Config::getCurrentTemplate('template');
+$jVars 				= array();
+$template 			= "template/{$currentTemplate}/dabout.html";
+
+require_once('views/modules.php');
+// redirect_to(BASE_URL.'shop');
+
+template($template, $jVars, $currentTemplate);
+
+?>
