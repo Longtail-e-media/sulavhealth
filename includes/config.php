0c16269c50bd58ae6bbddbab95095f2158708c2c
ARYAshak <aryashakya89@gmail.com>
2024-08-05 13:01:07 +0545

    UPDATE changes

--- includes/config.php
+++ includes/config.php
@@ -2,7 +2,7 @@
 
 $online = ($_SERVER['HTTP_HOST'] == "localhost" || $_SERVER['HTTP_HOST'] == "127.0.0.1" || $_SERVER['HTTP_HOST'] == "192.168.2.240") ? false : true;
 
-defined('SITE_FOLDER')  ? '' : define('SITE_FOLDER', '');
+defined('SITE_FOLDER')  ? '' : define('SITE_FOLDER', 'sulavhealth');
 defined('SITE_STR')     ? '' : define('SITE_STR', '');
 
 if ($online) { // ONLINE SETUP
