367c196fa0ee55a5d54116ee4d05271145881134
ARYAshak <aryashakya89@gmail.com>
2024-08-13 15:15:46 +0545

    [DESIGN] UI for readmore article page

--- views/module.articles.php
+++ views/module.articles.php
@@ -47,11 +47,11 @@ if (defined('INNER_PAGE') and isset($_REQUEST['slug'])) {
         $rescontent = explode('<hr id="system_readmore" style="border-style: dashed; border-color: orange;" />',
             trim(($recRow->content)));
         $content = (!empty($rescontent[1])) ? $rescontent[1] : $rescontent[0];
-        $resinndetail .= '<div class="container about mt-5 ">
-        <div class="row pt-60 pb-120">'.$content.'
-         </div>
-    </div>
-        ';
+        $resinndetail .= '  <div class="about-article">
+                                <div class="container">
+                                    '.$content.'
+                                </div>
+                            </div>';
     } else {
         redirect_to(BASE_URL);
     }
@@ -61,17 +61,21 @@ else{
     $resinndetail .= '
     <div class="ltn__utilize-overlay"></div>
 
-<div class="container mx-auto product-filter">
-    <div class="row">
-        <div class="col-md-12 product-filter-right">
-        <li class="first-mega">'.$recRow->title.'
-                            <ul>
-                                <li>'.$recRow->content.'</li>
-                            </ul>
-                            ';
+    <div class="about-article">
+        <div class="container mx-auto">
+            <div class="row">
+                <div class="col-md-9 product-filter-right">
+                    <h4>'.$recRow->title.'</h4>
+                    <p>'.$recRow->content.'</p>
+                </div>
+
+                <div class="col-md-3">
+                    <div class="related-topic-title">
+                    <h4>Related Topic</h4>
+                    ';
                             $menurelarticles = Article::menuArticle($recRow->prodcategory, $recRow->Subcategory, $recRow->service_id);
                             $resinndetail .= '
-                            <ul class="note-link">';
+                            <ul class="note-link-title">';
                             foreach($menurelarticles as $menurelarticle){
                             $resinndetail .= '
                                 <li><a href="'.BASE_URL.$menurelarticle->slug.'">'.$menurelarticle->title.'</a></li>
@@ -81,111 +85,111 @@ else{
                          $relatedproducts= SubProduct::menuproduct($recRow->prodcategory, $recRow->Subcategory, $recRow->service_id);
                         //  pr($relatedproducts);
                        $resinndetail .= ' </li>
-
-
-        </div>
-    </div>
-
-    <div class="row">
-        <div class="col-md-12 product-filter-right">
-            <h2><div id="searchtitle">related products</div></h2>
-            
-            <div id="productContainer" class="row">';
-            foreach($relatedproducts as $rows){
-
-                $price_text = '';
-            if (!empty($rows->price1) and (empty($rows->offer_price))) {
-                $price_text = '<span>' . $rows->currency . ' ' . $rows->price1 . '</span>';
-            }
-            if (!empty($rows->discount1)) {
-                $discountamt= $rows->price1 - $rows->discount1;
-                $price_text = '
-                <span>' . $rows->currency . ' '.$rows->discount1.'</span>|<span>' . $rows->discountedp . '%off</span><br/>
-                        <del>' . $rows->currency . ' ' . $rows->price1 . '</del> <span class="font-14">Save ' . $rows->currency . ' ' . $discountamt. '</span> 
-                
-                ';
-            }
-                $product = SubProduct::find_by_slug($rows->slug);
-            if (!empty($product)) {
-                $images = SubProductImage::getImagelist_by($product->id, 1, 0);
-                $img = BASE_URL . 'template/web/img/product/one.jpg';
-                if (!empty($images)) {
-                    foreach ($images as $image) {
-                        $file_path = SITE_ROOT . 'images/product/galleryimages/' . $image->image;
-                        if (file_exists($file_path)) {
-                            $img = IMAGE_PATH . 'product/galleryimages/' . $image->image;
-                        }
-                    }
-                }
-            }
-                $prodbrand = Brand::find_by_id($rows->brand);
-            $prodservice = Services::find_by_id($rows->service_id);
-            if (!empty($prodbrand)) {
-                $titlebrand = $prodbrand->title;
-                $slug = $prodbrand->slug;
-            } else {
-                $titlebrand = '';
-                $slug = '';
-            }
-            if (!empty($prodservice)) {
-                $slugs = '' . BASE_URL . 'product/' . $prodservice->slug . '/' . $rows->slug . '';
-            } else {
-                $slugs = '' . BASE_URL . 'product/product-detail/' . $rows->slug . '';;
-            }
-              $resinndetail .= '
-                <div class="col-xl-3 col-sm-6 col-6">
-                    <div class="ltn__product-item ltn__product-item-3 text-center">
-                        <a href="' . $slugs . '"><div class="product-img product_hove" data-href="' . $slugs . '">
-                            <img src="' . $img . '" alt="' . $rows->title . '">
-                        </div></a>
-                        <div class="product-info">
-                            <h4 class="product-title"><a href="' . BASE_URL . 'search/' . $slug . '" class="product-link">' . $titlebrand . '</a></h4>
-                            <a href="' . $slugs . '" class="product-link">' . $rows->title . '</a>
-                            <div class="product-price">
-                                ' . $price_text . '
-                            </div>
-                            <div class="product-action">
-            ';
-            if (!empty($rows->tag)) {
-                $resinndetail .= '<li class="sale-badge">' . $rows->tag. '</li>';
-            }
-            $resinndetail .= '
-                                <ul>
-                                    <li>
-                                        <a href="#" class="add-wishlist"
-                                            title="Add to Wishlist"
-                                            data-cartid="' . $rows->slug . '">
-                                            <i class="far fa-heart"></i>
-                                        </a>
-                                    </li>
-                                    <li>
-                                        <a href="#" title="ADD TO CART"
-                                            class="add-to-cart" data-toggle="modal"
-                                            data-target="#quick_view_modal_product_' . $rows->slug  . '">
-                                            Add to Cart
-                                            <i class="fas fa-shopping-cart"></i>
-                                        </a>
-                                    </li>
-                                </ul>
-                              </div>
-                        </div>
+                    </ul>
                     </div>
                 </div>
-            ';
-        }
-           $resinndetail .= '  </div>
-        </div>
-    </div>';
-    if($recRow->service_id != 0){
-        $servicerelated = Services::find_by_id($recRow->service_id);
-        $slugother= ''.BASE_URL.'product/'.$servicerelated->slug.'';
-    }
-    
-    $resinndetail .= ' <a href="'.$slugother.'">view more</a>
+            </div>
 
-</div>
-</div>
-</div>
+            <div class="space"></div>
+
+            <div class="row" style="margin-bottom:15px;">
+                <div class="col-md-12 product-filter-right">
+                    <h2><div id="searchtitle">Related products</div></h2>
+                    
+                    <div id="productContainer" class="row">';
+                    foreach($relatedproducts as $rows){
+
+                        $price_text = '';
+                    if (!empty($rows->price1) and (empty($rows->offer_price))) {
+                        $price_text = '<span>' . $rows->currency . ' ' . $rows->price1 . '</span>';
+                    }
+                    if (!empty($rows->discount1)) {
+                        $discountamt= $rows->price1 - $rows->discount1;
+                        $price_text = '
+                        <span>' . $rows->currency . ' '.$rows->discount1.'</span>|<span>' . $rows->discountedp . '%off</span><br/>
+                                <del>' . $rows->currency . ' ' . $rows->price1 . '</del> <span class="font-14">Save ' . $rows->currency . ' ' . $discountamt. '</span> 
+                        
+                        ';
+                    }
+                        $product = SubProduct::find_by_slug($rows->slug);
+                    if (!empty($product)) {
+                        $images = SubProductImage::getImagelist_by($product->id, 1, 0);
+                        $img = BASE_URL . 'template/web/img/product/one.jpg';
+                        if (!empty($images)) {
+                            foreach ($images as $image) {
+                                $file_path = SITE_ROOT . 'images/product/galleryimages/' . $image->image;
+                                if (file_exists($file_path)) {
+                                    $img = IMAGE_PATH . 'product/galleryimages/' . $image->image;
+                                }
+                            }
+                        }
+                    }
+                        $prodbrand = Brand::find_by_id($rows->brand);
+                    $prodservice = Services::find_by_id($rows->service_id);
+                    if (!empty($prodbrand)) {
+                        $titlebrand = $prodbrand->title;
+                        $slug = $prodbrand->slug;
+                    } else {
+                        $titlebrand = '';
+                        $slug = '';
+                    }
+                    if (!empty($prodservice)) {
+                        $slugs = '' . BASE_URL . 'product/' . $prodservice->slug . '/' . $rows->slug . '';
+                    } else {
+                        $slugs = '' . BASE_URL . 'product/product-detail/' . $rows->slug . '';;
+                    }
+                      $resinndetail .= '
+                        <div class="col-xl-3 col-sm-6 col-6">
+                            <div class="ltn__product-item ltn__product-item-3 text-center">
+                                <a href="' . $slugs . '"><div class="product-img product_hove" data-href="' . $slugs . '">
+                                    <img src="' . $img . '" alt="' . $rows->title . '">
+                                </div></a>
+                                <div class="product-info">
+                                    <h4 class="product-title"><a href="' . BASE_URL . 'search/' . $slug . '" class="product-link">' . $titlebrand . '</a></h4>
+                                    <a href="' . $slugs . '" class="product-link">' . $rows->title . '</a>
+                                    <div class="product-price">
+                                        ' . $price_text . '
+                                    </div>
+                                    <div class="product-action">
+                    ';
+                    if (!empty($rows->tag)) {
+                        $resinndetail .= '<li class="sale-badge">' . $rows->tag. '</li>';
+                    }
+                    $resinndetail .= '
+                                        <ul>
+                                            <li>
+                                                <a href="#" class="add-wishlist"
+                                                    title="Add to Wishlist"
+                                                    data-cartid="' . $rows->slug . '">
+                                                    <i class="far fa-heart"></i>
+                                                </a>
+                                            </li>
+                                            <li>
+                                                <a href="#" title="ADD TO CART"
+                                                    class="add-to-cart" data-toggle="modal"
+                                                    data-target="#quick_view_modal_product_' . $rows->slug  . '">
+                                                    Add to Cart
+                                                    <i class="fas fa-shopping-cart"></i>
+                                                </a>
+                                            </li>
+                                        </ul>
+                                      </div>
+                                </div>
+                            </div>
+                        </div>
+                    ';
+                }
+                   $resinndetail .= '  </div>
+                </div>
+            </div>';
+            if($recRow->service_id != 0){
+                $servicerelated = Services::find_by_id($recRow->service_id);
+                $slugother= ''.BASE_URL.'product/'.$servicerelated->slug.'';
+            }
+            
+            $resinndetail .= ' <a href="'.$slugother.'" class="article-botton">View All</a>
+        </div>
+    </div>
 
 <div id="productpopup">
 </div>';
