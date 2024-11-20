bf117ef819b79756abdfcaa3beaddd29d83c317e
Swarna Shakya <swarnamanshakya@gmail.com>
2024-11-20 16:23:02 +0545

    [ADD] Offers mgmt module

--- db/updates.sql
+++ db/updates.sql
@@ -39,3 +39,41 @@ CREATE TABLE `tbl_sites` (
 ALTER TABLE `tbl_sites` ADD PRIMARY KEY (`id`);
 ALTER TABLE `tbl_sites` MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 INSERT INTO `tbl_modules` (`id`, `parent_id`, `name`, `link`, `mode`, `icon_link`, `status`, `sortorder`, `added_date`, `properties`) VALUES (NULL, '0', 'Sites Mgmt', 'site/list', 'site', 'icon-list', '1', '6', '2024-11-20', '');
+
+CREATE TABLE `tbl_offers` (
+    `id` int(11) NOT NULL,
+    `slug` varchar(255) NOT NULL,
+    `title` varchar(100) NOT NULL,
+    `image` varchar(255) NOT NULL,
+    `list_image` varchar(255) NOT NULL,
+    `adults` int(11) NOT NULL,
+    `children` int(11) NOT NULL,
+    `linksrc` varchar(255) NOT NULL,
+    `linktype` tinyint(1) NOT NULL DEFAULT 0,
+    `rate` int(11) NOT NULL,
+    `discount` int(11) NOT NULL,
+    `brief` tinytext NOT NULL,
+    `content` longtext NOT NULL,
+    `status` int(1) NOT NULL DEFAULT 0,
+    `added_date` varchar(50) NOT NULL,
+    `sortorder` int(11) NOT NULL,
+    `type` tinyint(1) NOT NULL DEFAULT 0,
+    `offer_date` varchar(50) NOT NULL,
+    `start_date` varchar(50) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
+CREATE TABLE `tbl_offer_child` (
+    `offer_id` int(11) NOT NULL,
+    `offer_pax` varchar(200) NOT NULL,
+    `offer_usd` varchar(10) NOT NULL,
+    `offer_inr` varchar(10) NOT NULL,
+    `offer_npr` varchar(10) NOT NULL,
+    `offer_no` int(11) NOT NULL,
+    `multi_offer_title` varchar(255) NOT NULL,
+    `multi_offer_npr` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
+ALTER TABLE `tbl_offers` ADD PRIMARY KEY (`id`);
+ALTER TABLE `tbl_offer_child` ADD KEY `offer_id` (`offer_id`);
+ALTER TABLE `tbl_offers` MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+INSERT INTO `tbl_modules` (`id`, `parent_id`, `name`, `link`, `mode`, `icon_link`, `status`, `sortorder`, `added_date`, `properties`)
+VALUES (NULL, '0', 'Offers Mgmt', 'offers/list', 'offers', 'icon-tags', '1', '8', '2024-11-20', 'a:4:{s:9:\"bimgwidth\";s:2:\"22\";s:10:\"bimgheight\";s:2:\"22\";s:8:\"imgwidth\";s:3:\"200\";s:9:\"imgheight\";s:3:\"200\";}');
